4f77ec30d67df40dad6ae6b8843abd721eaec4ed
dencs08 <danonbizi@gmail.com>
2021-03-14 14:04:55 +0100

    package & composer updates

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="css/app.css">
         <title>Bison Design</title>
         
     </head>
     <body class="antialiased">
-        
+        <div id="root"></div>
+        <script src="js/app.js"></script>
     </body>
 </html>
