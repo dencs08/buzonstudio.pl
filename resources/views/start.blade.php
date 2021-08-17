59e871d1914a69e0ef5e479e3e003cd542e2be6e
dencs08 <daniel.buzon08@gmail.com>
2021-08-18 00:26:10 +0200

    view update

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/app.css">
-        <title>Bison Design</title>
-        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="css/app.css" />
+        <title>bisonstudio</title>
     </head>
     <body class="antialiased">
-        <div id="root"></div>
+        <!-- Navbar -->
+
+        <nav role="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href="#"><li>Info</li></a>
+                    <a href="#"><li>Contact</li></a>
+                    <a href="https://erikterwan.com/" target="_blank"
+                        ><li>Show me more</li></a
+                    >
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Navbar -->
+
+        <div class="landing-page">
+            <!-- <div class="container-fluid landing-page_content"> -->
+            <h1>Jesteśmy bisonstudio.</h1>
+            <!-- <p>Twoje kompletne rozwiązanie IT</p> -->
+            <!-- </div> -->
+        </div>
+
+        <canvas id="web_gl"></canvas>
+        <section class="scroller"></section>
+
         <script src="js/app.js"></script>
     </body>
 </html>
