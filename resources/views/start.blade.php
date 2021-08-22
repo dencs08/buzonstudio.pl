1f2f68568740071ffaf3d9dfe724b5bd15829a6a
dencs08 <59261630+dencs08@users.noreply.github.com>
2021-08-22 15:51:02 +0200

    view

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,41 +3,48 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="images/logo_white.svg" />
         <link rel="stylesheet" href="css/app.css" />
         <title>bisonstudio</title>
     </head>
     <body class="antialiased">
         <!-- Navbar -->
 
-        <nav role="navigation">
-            <div id="menuToggle">
-                <input type="checkbox" />
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul id="menu">
-                    <a href="#"><li>Home</li></a>
-                    <a href="#"><li>About</li></a>
-                    <a href="#"><li>Info</li></a>
-                    <a href="#"><li>Contact</li></a>
-                    <a href="https://erikterwan.com/" target="_blank"
-                        ><li>Show me more</li></a
-                    >
-                </ul>
-            </div>
-        </nav>
-
-        <!-- Navbar -->
-
-        <div class="landing-page">
-            <!-- <div class="container-fluid landing-page_content"> -->
-            <h1>Jesteśmy bisonstudio.</h1>
-            <!-- <p>Twoje kompletne rozwiązanie IT</p> -->
-            <!-- </div> -->
+        <input type="checkbox" id="active" />
+        <label for="active" class="menu-btn"><span></span></label>
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Feedback</a></li>
+            </ul>
         </div>
 
+        <!-- Landing Page -->
+
+        <div class="landing-page">
+            <div class="container-fluid fluid-margin">
+                <p class="landing-page_content_text">Cześć, jesteśmy</p>
+                <h1>bisonstudio.</h1>
+                <p class="landing-page_content_text_second">
+                    Twoje kompleksowe rozwiązanie na wszystkie technologiczne
+                    potrzeby!
+                </p>
+            </div>
+        </div>
+
+        <!-- WEBGL -->
         <canvas id="web_gl"></canvas>
-        <section class="scroller"></section>
+
+        <!-- Section1 -->
+
+        <div class="contaner-fluid fluid-margin">
+            <h2 class="we-create-header">
+                Tworzymy wspaniałe <span id="typed"></span>
+            </h2>
+        </div>
 
         <script src="js/app.js"></script>
     </body>
