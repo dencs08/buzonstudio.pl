07881fdd4b16351d4ffa5ccd133f5b4fdeb97d66
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-04 16:56:50 +0200

    update: start config of webpack to be ready for swup.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,60 +1,17 @@
 const mix = require('laravel-mix');
 
 mix.js([
-    'resources/js/gsapAnims.js',
-    'resources/js/navbar.js',
+    'resources/js/components/gsap/gsapAnims.js',
+    'resources/js/components/navbar.js',
     'resources/js/app.js'
 ], 'public/js/app.js').version()
-    .js([
-        'resources/js/components/start/start.js',
-        'resources/js/momentum-slider.js',
-        'resources/js/three.js',
-    ], 'public/js/start.js').version()
-    .js([
-        'resources/js/components/testimonials/testimonials.js',
-        'resources/js/landingpage-animation.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/testimonials.js').version()
-    .js([
-        'resources/js/typeAnim.js',
-        'resources/js/components/offer/offer.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/landingpage-animation.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/offer.js').version()
-    .js([
-        'resources/js/contactform.js',
-        'resources/js/components/contact/contact.js',
-        'resources/js/landingpage-animation.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/contact.js').version()
-    .js([
-        'resources/js/landingpage-animation.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/contactSent.js').version()
-    .js([
-        'resources/js/components/privacy/privacy.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/privacy.js').version()
-    .js([
-        'resources/js/components/portfolio/portfolio.js',
-        'resources/js/locomotive-scroll.js',
-        'resources/js/three_other_pages.js',
-    ], 'public/js/portfolio.js').version()
-    .js([
-        'resources/js/barba.js',
-    ], 'public/js/barba.js').version()
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/components/start.scss', 'public/css')
-    .sass('resources/sass/components/offer.scss', 'public/css')
-    .sass('resources/sass/components/testimonials.scss', 'public/css')
-    .sass('resources/sass/components/privacy.scss', 'public/css')
-    .sass('resources/sass/components/portfolio.scss', 'public/css')
-    .sass('resources/sass/components/contact.scss', 'public/css');
+    .sass('resources/sass/start.scss', 'public/css')
+    .sass('resources/sass/offer.scss', 'public/css')
+    .sass('resources/sass/testimonials.scss', 'public/css')
+    .sass('resources/sass/privacy.scss', 'public/css')
+    .sass('resources/sass/portfolio.scss', 'public/css')
+    .sass('resources/sass/contact.scss', 'public/css');
 
 mix.browserSync('127.0.0.1:8000');
 
