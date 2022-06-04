ef156268c2df752003b30672a061d26e76830cb1
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-04 16:57:06 +0200

    add: master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @yield('meta')
+        <link rel="canonical" href="https://www.bisonstudio.pl"/>
+        <link rel="apple-touch-icon" href="{{asset('images/logos/favicon.png')}}">
+        <link rel="shortcut icon" href="{{asset('images/logos/favicon.png')}}" />
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+
+        @yield('css')
+        <title>@yield('title')</title>
+    </head>
+    <body class="antialiased">
+
+        <div id="swup" class="transition-fade">
+            <canvas id="web_gl"></canvas>
+            @yield('content')
+            @yield('js')
+        </div>
+    </body>
+    <!-- <script src="{{asset('js/app.js')}}"></script> -->
+</html>
