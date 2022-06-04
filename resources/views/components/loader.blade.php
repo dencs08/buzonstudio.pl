9c4d97f952a51ca95569f6f03ccabb304d177305
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-04 16:57:17 +0200

    remove: courtain from loader.blade

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,21 +1,13 @@
 <div class="courtain">
-    <!-- <div class="progress" id="PreLoaderBar">
-        <div class="indeterminate"></div>
-    </div> -->
-    
     <div id="cursor" class="cursor"></div>
-<div id="cursor-follower" class="cursor-follower"></div>
-
+    <div id="cursor-follower" class="cursor-follower"></div>
 
     <div class="courtain-wrapper">
-            <a href="#" id="start-button" class="underline">WEJDŹ</a>
-            <div class="privacy-policy-start">
-                <span class="info-accept mx-1">Wejście i korzystanie ze strony jest jednoznaczne z zaakceptowaniem ciasteczek oraz naszej</span>
-                <a href="polityka" class="privacy-policy underline web_link_transitions">polityki prywatności</a>
-            </div>
+        <a href="#" id="start-button" class="underline">WEJDŹ</a>
+        <div class="privacy-policy-start">
+            <span class="info-accept mx-1">Wejście i korzystanie ze strony jest jednoznaczne z zaakceptowaniem ciasteczek oraz naszej</span>
+            <a href="polityka" class="privacy-policy underline web_link_transitions">polityki prywatności</a>
         </div>
     </div>
-    <div class="top-side"></div>
-    <div class="bottom-side"></div>
 </div>
 
