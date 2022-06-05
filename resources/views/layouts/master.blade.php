b4364dcd4b54431504403a2a4a012093c2240624
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-05 14:56:01 +0200

    update: master.blade new favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,14 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @yield('meta')
         <link rel="canonical" href="https://www.bisonstudio.pl"/>
-        <link rel="apple-touch-icon" href="{{asset('images/logos/favicon.png')}}">
-        <link rel="shortcut icon" href="{{asset('images/logos/favicon.png')}}" />
+        <link rel="apple-touch-icon" href="{{asset('images/logos/favicon/buzonstudio_favicon_green.png')}}">
+        <link rel="shortcut icon" href="{{asset('images/logos/favicon/buzonstudio_favicon_green.png')}}" />
         <link rel="stylesheet" href="{{asset('css/app.css')}}" />
 
         @yield('css')
         <title>@yield('title')</title>
     </head>
     <body class="antialiased">
+        <x-loader/>
 
         <div id="swup" class="transition-fade">
             <canvas id="web_gl"></canvas>
