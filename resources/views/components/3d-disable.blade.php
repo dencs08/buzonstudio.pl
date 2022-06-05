aef4700eab61e1c85fff17202771d90ae95d571e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-05 14:56:51 +0200

    remove: unused component.blade

--- resources/views/components/3d-disable.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="three_disabler_container">
-    <a href="#" id="three_disabler" class="three_disabler underline">Wyłącz 3D</a>
-    <a href="#" id="three_enabler" class="three_enabler underline">Włącz 3D</a>
-</div>
