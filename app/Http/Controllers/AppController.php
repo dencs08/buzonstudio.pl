97a4a33d49501cb13e3892c3a7345ca3a559fea5
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-10 03:01:10 +0200

    update: app & portfolio controller pass content to view

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Portfolio;
 
 class AppController extends Controller
 {
     function start(){
-        return view('start');
+        $request = Portfolio::findMany([1,11,5,3,2])->sortBy('name');
+        $portfolio_data = json_decode($request, false);
+
+        return view('start', [
+            'portfolio_data' => $portfolio_data
+        ]);
     }
 
     function offer(){
         return view('offer');
-    }
-
-    function portfolio(){
-        return view('portfolio');
     }
 
     function contact(){
--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,28 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
+use App\Models\Responsibilities;
+use App\Models\Goals;
+use App\Models\Images;
 
 class PortfolioController extends Controller
 {
+    public function portfolio($name){
+        // $portfolio_data = Portfolio::all();
+        // $data = $this->data;
 
-    public $data = [
+        $project = Portfolio::getByName($name);
+        $images = Images::getByName($name);
+        $responsibilities = Responsibilities::getByName($name);
+        $goals = Goals::getByName($name);
 
-    ];
-
-    public function offerShow(){
-        $data = $this->data;
-        
-        return view('oferta', [
-            'portfolios' => $data
-        ]);
-    }
-    
-    public function show($name){
-        $portfolio_data = Portfolio::all();
-        $data = $this->data;
-
-        return view('portfolio', [
-            // 'portfolioName' => $data["name"] ?? 'Portfolio o nazwie ' . $name . ' nie istnieje'
+        return view('project', [
+            'name' => $project->name ?? 'Portfolio o nazwie ' . $name . ' nie istnieje',
+            'website' => $project->website ?? '',
+            'info' => $project->info ?? '',
+            'responsibilities' => $responsibilities ?? '',
+            'goals' => $goals ?? '',
+            'images' => $images ?? ''
         ]);
     }
 }
