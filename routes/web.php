a484846ec32eebda9dde816c948f3dcf3ff0dca7
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-10 03:15:47 +0200

    update: routes to match new controller

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\AppController::class, 'start']);
 Route::get('start', [App\Http\Controllers\AppController::class, 'start']);
-Route::get('portfolio', [App\Http\Controllers\AppController::class, 'portfolio']);
 Route::get('oferta', [App\Http\Controllers\AppController::class, 'offer']);
 Route::get('polityka', [App\Http\Controllers\AppController::class, 'privacy']);
 Route::get('kontakt', [App\Http\Controllers\EmailController::class, 'view']);
 Route::post('/kontaktsent', [App\Http\Controllers\EmailController::class, 'sendEmail'])->name('send.email');
+Route::get('portfolio', [App\Http\Controllers\AppController::class, 'portfolio']);
+Route::get('portfolio/{name}', [App\Http\Controllers\AppController::class, 'project']);
+
 // Route::get('start', function () {
 //     return view('start');
 // });
-
 
 // Route::get('oferta', function () {
 //     return view('oferta');
