8a1ec4821705dd6d5551b34170071c865bc51df0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-13 17:15:45 +0200

    fix: email controller use env variables instead of hard-coded

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -26,23 +26,23 @@ class EmailController extends Controller
           'email' => $request->email,
           'content' => $request->content,
           'controlInfo' => $request->controlInfo,
-          // 'logo' => asset('images/logos/logo_black.svg')
+          // 'logo' => asset('/images/logos/buzonstudio_black.svg')
         ];
 
-        Mail::send('email-template', $data, function($message) use ($data) {
-          $message->to("biuro@bisonstudio.pl")
-          ->subject("Nowa wiadomość ze strony bisonstudio")
-          ->from("biuro@bisonstudio.pl", "Bisonstudio")
-          ->sender("biuro@bisonstudio.pl", "Bisonstudio");
+        Mail::send('emails.email-template', $data, function($message) use ($data) {
+          $message->to(env('MAIL_USERNAME'))
+          ->subject("Nowa wiadomość ze strony buzonstudio")
+          ->from(env('MAIL_USERNAME'), env('APP_NAME'))
+          ->sender(env('MAIL_USERNAME'), env('APP_NAME'));
         });
 
-        Mail::send('email-template-user-confirm', $data, function($message) use ($data) {
+        Mail::send('emails.email-template-user-confirm', $data, function($message) use ($data) {
           $message->to($data['email'])
           ->subject("Dziękujemy za kontakt!")
-          ->from("biuro@bisonstudio.pl", "Bisonstudio")
-          ->sender("biuro@bisonstudio.pl", "Bisonstudio");
+          ->from(env('MAIL_USERNAME'), env('APP_NAME'))
+          ->sender(env('MAIL_USERNAME'), env('APP_NAME'));
         });
 
-        return view('email-sent');
+        return view('emails.email-sent');
     }
 }
