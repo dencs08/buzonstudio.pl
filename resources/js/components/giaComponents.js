6af682a3672fe7708c9b7797f98b70fc6dc78727
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-18 00:18:59 +0200

    update: giaComponents with new classes

--- resources/js/components/giaComponents.js
+++ resources/js/components/giaComponents.js
@@ -1,10 +1,14 @@
 import Navbar from './navbar'
 import Locomotive from './locomotive-scroll'
+import Cursor from './cursor'
+import PortfolioSlider from './slider'
 import Portfolio from '../portfolio'
 
 const components = {
     Navbar,
     Locomotive,
+    Cursor,
+    PortfolioSlider,
     Portfolio,
 }
 
