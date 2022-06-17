a8a6f8ca7a3e3ef145471cec516fef40fd6443ff
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-17 16:45:08 +0200

    fix: locomotive scroll & animations now works when going between pages

--- resources/js/components/locomotive-scroll.js
+++ resources/js/components/locomotive-scroll.js
@@ -3,98 +3,115 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import LocomotiveScroll from 'locomotive-scroll';
 import Component from 'gia/Component';
 
+let locoScroll;
 export default class Locomotive extends Component {
+    async require() {
+        this.throttle = await import('locomotive-scroll');
+        this.throttle = await import('gsap');
+        this.throttle = await import('gsap/ScrollTrigger');
+    }
+
     constructor(element) {
         super(element)
+
+        document.addEventListener("DOMContentLoaded", function () {
+            create()
+        });
     }
 
     mount() {
-        gsap.registerPlugin(ScrollTrigger);
 
-        const locoScroll = new LocomotiveScroll({
-            el: document.querySelector('[data-locomotive-scroll]'),
-            smooth: true,
-            lerp: .085,
-            // firefoxMultiplier: 50,
-            marker: true
-        });
-
-        locoScroll.on("scroll", ScrollTrigger.update);
-
-        ScrollTrigger.scrollerProxy("[data-locomotive-scroll]", {
-            scrollTop(value) {
-                return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-            },
-            getBoundingClientRect() {
-                return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-            },
-            pinType: document.querySelector("[data-locomotive-scroll]").style.transform ? "transform" : "fixed"
-        });
-
-        //!auto animations
-        function animateFrom(elem, direction) {
-            direction = direction || 1;
-            var x = 0,
-                y = direction * 100;
-            if (elem.getAttribute("[data-gs_fromLeft]")) {
-                x = -100;
-                y = 0;
-            } else if (elem.getAttribute("[data-gs_fromRight]")) {
-                x = 100;
-                y = 0;
-            } else if (elem.getAttribute("[data-gs_fromBottom]")) {
-                y = 50;
-                x = 0;
-            } else if (elem.getAttribute("[data-gs_fromTop]")) {
-                y = -50;
-                x = 0;
-            } else if (elem.getAttribute("[data-gs_fromFadeIn]")) {
-                y = 0;
-                x = 0;
-            }
-            elem.style.transform = "translate(" + x + "px, " + y + "px)";
-            elem.style.opacity = "0";
-
-            gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
-                autoAlpha: 1,
-                duration: 1,
-                ease: "expo",
-                y: 0,
-                x: 0,
-                delay: 0.25
-            });
-        }
-
-        function hide(elem) {
-            gsap.set(elem, { autoAlpha: 0 });
-        }
-
-        document.addEventListener("DOMContentLoaded", function () {
-            gsap.registerPlugin(ScrollTrigger);
-
-            gsap.utils.toArray("[data-gs]").forEach(function (elem) {
-                hide(elem);
-
-                ScrollTrigger.create({
-                    trigger: elem,
-                    start: "top 100%",
-                    // end: "bottom 80%+=100px",
-                    once: true,
-                    scroller: "[data-locomotive-scroll]",
-                    // markers: true,
-                    onEnter: function () { animateFrom(elem) },
-                });
-            });
-        });
-
-        ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-        ScrollTrigger.refresh();
     }
 
     unmount() {
-
+        locoScroll.destroy()
+        // locoScroll = null;
     }
 }
+
+function create() {
+    locoScroll = new LocomotiveScroll({
+        el: document.querySelector('[data-locomotive-scroll]'),
+        smooth: true,
+        lerp: .085,
+        // firefoxMultiplier: 50,
+    });
+
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.scrollerProxy("[data-locomotive-scroll]", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector("[data-locomotive-scroll]").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray("[data-gs]").forEach(function (elem) {
+        hide(elem);
+        ScrollTrigger.create({
+            trigger: elem,
+            start: "top bottom",
+            end: "top top",
+            once: true,
+            scroller: "[data-locomotive-scroll]",
+            markers: true,
+            onEnter: function () { animateFrom(elem) },
+        });
+    });
+}
+
+//!auto animations
+function animateFrom(elem, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if (elem.getAttribute("[data-gs_fromLeft]")) {
+        x = -100;
+        y = 0;
+    } else if (elem.getAttribute("[data-gs_fromRight]")) {
+        x = 100;
+        y = 0;
+    } else if (elem.getAttribute("[data-gs_fromBottom]")) {
+        y = 50;
+        x = 0;
+    } else if (elem.getAttribute("[data-gs_fromTop]")) {
+        y = -50;
+        x = 0;
+    } else if (elem.getAttribute("[data-gs_fromFadeIn]")) {
+        y = 0;
+        x = 0;
+    }
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+
+    gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
+        autoAlpha: 1,
+        duration: 1,
+        ease: "expo",
+        y: 0,
+        x: 0,
+        delay: 0.25
+    });
+}
+
+function reload() {
+    locoScroll.update()
+    ScrollTrigger.refresh()
+}
+
+function hide(elem) {
+    gsap.set(elem, { autoAlpha: 0 });
+}
+
+export { create as locoCreate, reload as locoReload }
 
 // if (location.pathname == "/kontakt") {
 //     const subjectButtonsGroup = document.querySelectorAll(".control-group")
